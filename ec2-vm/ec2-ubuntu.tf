b5a4f5713ba7832390d9aaa43a7e039503028b30
Christian Ferber <cferber@vmware.com>
2020-09-17 13:52:43 +0200

    ec2VM to us-east-1

--- ec2-vm/ec2-ubuntu.tf
+++ ec2-vm/ec2-ubuntu.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-0dba2cb6798deb6d8"
   instance_type = "t2.micro"
 }
