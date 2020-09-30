e8fc38f3cea77fcbb05dbb497cdfc6da5215c2cf
Christian Ferber <cferber@vmware.com>
2020-09-30 17:54:01 +0200

    using variable defaults

--- vra-item/variables.tf
+++ vra-item/variables.tf
@@ -1,11 +1,28 @@
-variable "url" {}
+variable "url" {
+  type = string
+  default = "https://vra801.space.local"
+}
 
-variable "refresh_token" {}
+variable "refresh_token" {
+  type = string
+  default = "knGOXLVzzwvb733l1hJpwFYHrRDHHogQ"
+}
 
-variable "insecure" {}
+variable "insecure" {
+  type = string
+  default = "true"
+}
 
-variable "deployment_name" {}
+variable "deployment_name" {
+  type = string
+}
 
-variable "blueprint_name" {}
+variable "blueprint_name" {
+  type = string
+  default = "CentOS Server"
+}
 
-variable "project_name" {}
+variable "project_name" {
+  type = string
+  default = "IT-Services"
+}
