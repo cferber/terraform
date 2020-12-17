18bbde5afcd2f75f5be5c4901e2c1d1a6f687513
cferber <cferber@vmware.com>
2020-12-17 11:53:25 +0100

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "refresh_token" {
+}
+
+variable "url" {
+}
+
+variable "insecure" {}
+
+variable "zone_name" {
+}
+
+variable "project_name" {}
+
+variable "usertoadd" {}
+
+variable "catalog_source_id" {}
