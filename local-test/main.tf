867a4795a3fd33f60f325eb0b550cd4003a55462
Code Stream <cferber@vmware.com>
2021-01-11 09:33:09 +0100

    curl test

--- local-test/main.tf
+++ local-test/main.tf
@@ -1,7 +1,15 @@
 resource "null_resource" "download-file" {
   provisioner "local-exec" {
     command = <<EOT
-    cat /etc/*release*
+    apk --no-cache add curl
     EOT
   }
+}
+
+resource "null_resource" "test-local" {
+  provisioner "local-exec" {
+    command = <<EOT
+    curl -Is https://www.google.de | head -n 1
+    EOT
+  } 
 }
